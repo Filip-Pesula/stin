c9deda5a1e766c55c0dee352a5bee1446c5261e4
Filip-Pesula <filip.pesula@users.noreply.github.com>
2022-05-05 10:29:07 +0000

    Automated report

--- build/Debug/ui/BotClient.js
+++ build/Debug/ui/BotClient.js
@@ -12,6 +12,13 @@ function submitForm() {
     http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
     var params = document.getElementById('botInputForm').value; // probably use document.getElementById(...).value
     console.log(params);
+
+    var ul = document.getElementById("messageList");
+    var li = document.createElement("li");
+    li.classList.add('req');
+    li.appendChild(document.createTextNode(params));
+    ul.appendChild(li);
+
     http.send(params);
     http.onload = function() {
         alert(http.responseText);
@@ -21,6 +28,7 @@ function alert(responseText){
     console.log(responseText)
     var ul = document.getElementById("messageList");
     var li = document.createElement("li");
+    li.classList.add('res');
     li.appendChild(document.createTextNode(responseText));
     ul.appendChild(li);
 }
